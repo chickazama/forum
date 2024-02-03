1a7087339ce0dded1c1584c3f596cf88b91c2c0c
Matthew Hope <matt@matthewhope.co.uk>
2024-02-03 18:34:45 +0000

    Define UsersTableUp function

---
+++ sqlite/UsersTableUp.go
@@ -0,0 +1,26 @@
+package sqlite
+
+import "database/sql"
+
+func UsersTableUp(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS USERS (
+		UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+		CreatedAt BIG INTEGER NOT NULL,
+		EmailAddress TEXT UNIQUE NOT NULL,
+		EncryptedPassword TEXT NOT NULL,
+		FirstName TEXT NOT NULL,
+		LastName TEXT NOT NULL,
+		UpdatedAt BIG INTEGER NOT NULL,
+		Username TEXT NOT NULL
+	);`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec()
+	if err != nil {
+		return err
+	}
+	return nil
+}
