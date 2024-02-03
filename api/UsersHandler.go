f98ebdbffd0ca500ef184de8a06ae248d56f44ea
Matthew Hope <matt@matthewhope.co.uk>
2024-02-03 19:54:42 +0000

    Implement HTTP GET on api.UsersHandler

--- api/UsersHandler.go
+++ api/UsersHandler.go
@@ -1,6 +1,11 @@
 package api
 
-import "matthewhope/forum/repo"
+import (
+	"encoding/json"
+	"log"
+	"matthewhope/forum/repo"
+	"net/http"
+)
 
 type UsersHandler struct {
 	Repo repo.IRepository
@@ -8,4 +13,30 @@ type UsersHandler struct {
 
 func NewUsersHandler(r repo.IRepository) *UsersHandler {
 	return &UsersHandler{Repo: r}
+}
+
+func (h *UsersHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.get(w, r)
+		return
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func (h *UsersHandler) get(w http.ResponseWriter, r *http.Request) {
+	ret, err := h.Repo.GetAllUsers()
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(&ret)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
 }
