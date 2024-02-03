268b852475e2c6a0363e5685dfb11617fe720537
Matthew Hope <matt@matthewhope.co.uk>
2024-02-03 19:03:25 +0000

    Define SQLite PostsTableUp

---
+++ sqlite/PostsTableUp.go
@@ -0,0 +1,24 @@
+package sqlite
+
+import "database/sql"
+
+func PostsTableUp(db *sql.DB) error {
+	query := `CREATE TABLE IF NOT EXISTS "POSTS" (
+		PostID INTEGER PRIMARY KEY AUTOINCREMENT,
+		Category TEXT NOT NULL,
+		Content TEXT NOT NULL,
+		CreatedAt BIG INTEGER NOT NULL,
+		UpdatedAt BIG INTEGER NOT NULL,
+		UserID INTEGER NOT NULL
+	);`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec()
+	if err != nil {
+		return err
+	}
+	return nil
+}
