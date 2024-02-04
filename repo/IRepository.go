5ebc733ae4c9ad028e69b8bd9897ce1fcf1fe240
Matthew Hope <matt@matthewhope.co.uk>
2024-02-04 11:54:33 +0000

    Fix: Return single Post model, not slice, in CreatePost repo interface method

--- repo/IRepository.go
+++ repo/IRepository.go
@@ -6,5 +6,5 @@ type IRepository interface {
 	GetAllUsers() ([]models.User, error)
 	CreateUser(models.User) (models.User, error)
 	GetAllPosts() ([]models.Post, error)
-	CreatePost(models.Post) ([]models.Post, error)
+	CreatePost(models.Post) (models.Post, error)
 }
