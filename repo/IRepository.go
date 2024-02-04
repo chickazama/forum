43224cacd2944c5908a0a65c25f767af2e86ee39
Matthew Hope <matt@matthewhope.co.uk>
2024-02-04 11:37:39 +0000

    Define GetAllPosts repo interface method

--- repo/IRepository.go
+++ repo/IRepository.go
@@ -5,4 +5,5 @@ import "matthewhope/forum/models"
 type IRepository interface {
 	GetAllUsers() ([]models.User, error)
 	CreateUser(models.User) (models.User, error)
+	GetAllPosts() ([]models.Post, error)
 }
