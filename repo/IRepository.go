abab8d7c65fb97d0134923ec0af6057714f22fb8
Matthew Hope <chickazama@gmail.com>
2024-02-22 10:18:50 +0000

    Define TableUp repo prototypes

--- repo/IRepository.go
+++ repo/IRepository.go
@@ -3,6 +3,8 @@ package repo
 import "matthewhope/forum/models"
 
 type IRepository interface {
+	PostsTableUp() error
+	UsersTableUp() error
 	GetAllUsers() ([]models.User, error)
 	CreateUser(models.User) (models.User, error)
 	GetAllPosts() ([]models.Post, error)
